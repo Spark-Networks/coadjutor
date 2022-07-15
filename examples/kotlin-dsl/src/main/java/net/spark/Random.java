5a9df5e2de45e0fcadf5a86cf6505e85769ee732
Bhupendrakumar Piprava <bhupendrakumar.piprava@spark.net>
2022-07-15 09:19:05 +0200

    Deleted local examples

--- examples/kotlin-dsl/src/main/java/net/spark/Random.java
+++
@@ -1,7 +0,0 @@
-package net.spark;
-
-public class Random {
-    public static int between(int min, int max) {
-        return (int) ((Math.random() * (max - 1 - min)) + min);
-    }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,2 @@
 rootProject.name = "spark-gradle-plugins"
-
-pluginManagement {
-    includeBuild("./plugin")
-
-    repositories {
-        gradlePluginPortal()
-    }
-}
-
-include("examples:kotlin-dsl")
-include("examples:groovy-dsl")
+include(":plugin")
